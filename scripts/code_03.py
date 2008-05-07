f3a50a14796a2e7127a94e5107518f3340e05b40
Paulo Nuin <pnuin@etillier4.uhnres.utoronto.ca>
2008-05-07 15:13:13 -0400

    three more scripts added

---
+++ scripts/code_03.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+'''third script, shows how to import a module and use the 
+regex module to transcribe DNA to RNA
+'''
+
+#import regular expression module
+import re
+
+#setting the DNA string
+myDNA = 'ACGTTGCAACGTTGCAACGTTGCA'
+
+#assigning a new regex and compiling it 
+#to find all Ts
+regexp = re.compile('T')
+
+#create a new string tha will receive 
+#the regex result with Us replacing Ts
+myRNA = regexp.sub('U', myDNA)
+
+print myRNA
