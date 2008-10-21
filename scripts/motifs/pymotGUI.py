50bbbaa3b77e6af8bdd84f6a08b8a4ddbf8526c7
Paulo Nuin <nuin@genedrift.org>
2008-10-21 12:48:42 -0400

    added menu to the interface

--- scripts/motifs/pymotGUI.py
+++ scripts/motifs/pymotGUI.py
@@ -6,8 +6,8 @@ import fasta
 
 class pymot(wx.App):
 
-    def __init__(self, redirect=False, filename=None):
-        wx.App.__init__(self, redirect, filename)
+    def __init__(self, redirect=False):
+        wx.App.__init__(self, redirect)
 
 class pymotGUI(wx.Frame):
     
@@ -18,7 +18,20 @@ class pymotGUI(wx.Frame):
 #        self.__do_binding()
 
     def __do_layout(self):
-        pass
+        #defines the menubar
+        menubar = wx.MenuBar()
+        
+        #file menu
+        filemenu = wx.Menu()
+        self.convertmenu = filemenu.Append(-1, 'Select foreground file')
+        self.seqmenu = filemenu.Append(-1, 'Select background file')
+        self.treemenu = filemenu.AppendSeparator()
+        self.treenooutmenu = filemenu.Append(-1, 'Quit')
+        
+        #appends the menu to the menubar and creates it
+        menubar.Append(filemenu, 'File')
+        self.SetMenuBar(menubar)
+
 
 #if __name__ == '__main__':
 app = pymot()
