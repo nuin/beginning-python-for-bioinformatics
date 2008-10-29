34a71066040231275fbec92996211d9cd8986877
Paulo Nuin <nuin@genedrift.org>
2008-10-29 15:29:29 -0400

    added menu functions, events

--- scripts/motifs/pymotGUI.py
+++ scripts/motifs/pymotGUI.py
@@ -12,10 +12,9 @@ class pymot(wx.App):
 class pymotGUI(wx.Frame):
     
     def __init__(self, parent, id):
-        
         wx.Frame.__init__(self, parent, id,  'Python Motif Finder', style=wx.DEFAULT_FRAME_STYLE)
         self.__do_layout()
-#        self.__do_binding()
+        self.__do_binding()
 
     def __do_layout(self):
         
@@ -27,15 +26,23 @@ class pymotGUI(wx.Frame):
         
         #file menu
         filemenu = wx.Menu()
-        convertmenu = filemenu.Append(-1, 'Select foreground file')
-        seqmenu = filemenu.Append(-1, 'Select background file')
+        foreground_menu = filemenu.Append(-1, 'Select foreground file')
+        background_menu = filemenu.Append(-1, 'Select background file')
         sep = filemenu.AppendSeparator()
-        treenooutmenu = filemenu.Append(-1, 'Quit')
+        quitmenu = filemenu.Append(-1, 'Quit')
         
         #appends the menu to the menubar and creates it
         menubar.Append(filemenu, 'File')
         self.SetMenuBar(menubar)
 
+        self.Bind(wx.EVT_MENU, self.on_foreground, foreground_menu)
+        self.Bind(wx.EVT_MENU, self.on_background, background_menu)
+        
+    def on_foreground(self, event):
+        pass
+
+    def on_background(self, event):
+        pass
 
 #if __name__ == '__main__':
 app = pymot()
